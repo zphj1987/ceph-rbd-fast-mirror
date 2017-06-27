e3da1c39f9d569b2cdd19a3a8c3a51e6d8711290
Peng Zhang <zphj1987@gmail.com>
2017-06-27 16:25:35 +0800

    还可以用的版本

--- test.py
+++ test.py
@@ -10,65 +10,109 @@ import urwid
 import commands
 import urwid.raw_display
 
+
+####################
+class PopUpDialog(urwid.WidgetWrap):
+    """A dialog that appears with nothing but a close button """
+    signals = ['close']
+    def __init__(self):
+        close_button = urwid.Button("that's pretty cool")
+        urwid.connect_signal(close_button, 'click',
+            lambda button:self._emit("close"))
+        pile = urwid.Pile([urwid.Text(
+            "^^  I'm attached to the widget that opened me. "
+            "Try resizing the window!\n"), close_button])
+        fill = urwid.Filler(pile)
+        self.__super.__init__(urwid.AttrWrap(fill, 'popbg'))
+
+class ThingWithAPopUp(urwid.PopUpLauncher):
+    def __init__(self):
+        self.__super.__init__(urwid.Button("click-me"))
+        urwid.connect_signal(self.original_widget, 'click',
+            lambda button: self.open_pop_up())
+
+    def create_pop_up(self):
+        pop_up = PopUpDialog()
+        urwid.connect_signal(pop_up, 'close',
+            lambda button: self.close_pop_up())
+        return pop_up
+
+    def get_pop_up_parameters(self):
+        return {'left':0, 'top':1, 'overlay_width':32, 'overlay_height':7}
+
+####################
+
 def main():
 
     def read_conf(object):
+        """
+        读取目标配置文件传入的是要读取的配置文件
+        """
         getconf=commands.getoutput('ceph-conf --lookup -c /ceph.conf %s 2>/dev/null' %object)
         return getconf
+    
+    
     def write_conf(content,object):
+        """
+        写入配置文件：
+        content为写入的内容，object为写入的配置字段
+        """       
         writeconf=commands.getoutput('sed -i "s/%s=.*/%s=%s/g" /ceph.conf  2>/dev/null' %(object,object,content))
-        #writeconf=commands.getoutput('echo %s %s %s' %(object,object,content))
-        return writeconf
-
     text_header = (u"欢迎使用fast copy  "
         u"可以使用 F8 exits.")
     text_footer = (u"Welcome to the urwid tour!  "
         u"UP / DOWN / PAGE UP / PAGE DOWN scroll.  jiao.")
     text_intro = [('important', u"Text"),
-        u" widgets are the most common in "
-        u"any urwid program.  This Text widget was created "
-        u"without setting the wrap or align mode, so it "
-        u"defaults to left alignment with wrapping on space "
-        u"characters.  ",
-        ('important', u"Change the window width"),
-        u" to see how the widgets on this page react.  "
-        u"This Text widget is wrapped with a ",
-        ('important', u"Padding"),
-        u" widget to keep it indented on the left and right."]
-    text_edit_origin_mon = ('editcp', u"输入原ceph集群的monip: ")
+        u"本工具是用于快速的复制 "
+        u"一个RBD到另外的一个集群的"]
+    text_edit_origin_mon = ('editcp', u"输入原始ceph集群的monip: ")
     text_edit_dest_mon = ('editcp', u"输入目标ceph集群的monip: ")
     text_edit_text1 = read_conf('originmon')
-    text_edit_text2 = u"A"
-    reply = urwid.Text(u"")
+    text_edit_text2 = read_conf('destmon')
+    
     button = urwid.Button(u'保存配置')
     blank = urwid.Divider()
     editoriginmon=urwid.Edit(text_edit_origin_mon,text_edit_text1)
-    savebutton=urwid.AttrWrap(urwid.Button(u'保存配置'),'buttn')
+    editdestmon=urwid.Edit(text_edit_dest_mon,text_edit_text2)
+    savebutton=urwid.AttrWrap(button,'buttn')
     listbox_content = [
         urwid.Padding(urwid.Text(text_intro), left=2, right=2, min_width=40),
-        urwid.AttrWrap(editoriginmon,
-            'editbx', 'editfc'),
+        urwid.Padding(urwid.AttrWrap(editoriginmon,
+            'editbx', 'editfc'),left=2, width=50),
         blank,
-        urwid.AttrWrap(urwid.Edit(text_edit_dest_mon,text_edit_text2),
-            'editbx', 'editfc'),
-        urwid.Padding(savebutton,'center',20),
-        reply,
+        urwid.Padding(urwid.AttrWrap(editdestmon,
+            'editbx', 'editfc'),left=2, width=50),
+        blank,
+        urwid.Padding(savebutton,left=2,width=12),
         ]
+
+
 
     def on_exit_clicked(button):
 #        raise urwid.ExitMainLoop()
         try:
-            write_conf(myedit,'originmon')
-            print  write_conf(myedit,'originmon')
+            if globals().has_key('myorinedit'):
+                write_conf(myorinedit,'originmon')
+            if globals().has_key('mydestedit'):
+                write_conf(mydestedit,'destmon')  
+            ThingWithAPopUp()         
         except:
             pass
 
+    def on_save_change(edit, edit_addr,mysave):
+        if mysave == 'origin':
+            global myorinedit
+            myorinedit = edit_addr
+        elif mysave == 'dest':
+            global mydestedit
+            mydestedit = edit_addr
 
-    def on_save_change(edit, edit_addr):
-        global myedit
-        myedit= edit_addr
+        
+
+    
     urwid.connect_signal(button, 'click', on_exit_clicked)
-    urwid.connect_signal(editoriginmon, 'change', on_save_change)
+    urwid.connect_signal(editoriginmon, 'change', on_save_change,'origin')
+    urwid.connect_signal(editdestmon, 'change', on_save_change,'dest')
 
 
     #AttrWrap是用来给文本设置颜色，属性
@@ -77,6 +121,7 @@ def main():
     
 
     listbox = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
+#    listbox = urwid.Filler(urwid.Padding(ThingWithAPopUp(), 'center', 15))
     frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header,footer=footer)
 
     palette = [
@@ -101,7 +146,7 @@ def main():
             raise urwid.ExitMainLoop()
 
     urwid.MainLoop(frame, palette, screen,
-        unhandled_input=unhandled).run()
+        unhandled_input=unhandled,pop_ups=True).run()
 
 def setup():
 
